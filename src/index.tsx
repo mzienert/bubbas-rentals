93f2289b8ba645f491431833c0c5fe0a8c64778f
Matthew Zienert <mzienert@gmail.com>
2020-11-24 18:32:44 -0700

    wtf

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from "./redux/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = () =>
+    ReactDOM.render(
+      <React.StrictMode>
+          <Provider store={store}>
+            <App />
+          </Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('./components/App', renderApp)
+}
+
+renderApp()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
