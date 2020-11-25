444352e1641910e52255adddaf3f06218f1ad164
Matthew Zienert <mzienert@gmail.com>
2020-11-24 19:47:00 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./redux/store";
+import 'fontsource-roboto'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const darkTheme = createMuiTheme({
+  palette: {
+      type: 'dark',
+  },
+})
 
 const renderApp = () =>
     ReactDOM.render(
       <React.StrictMode>
           <Provider store={store}>
-            {/* <App /> */}
+            <ThemeProvider theme={darkTheme}>
+              <App />
+            </ThemeProvider>
           </Provider>
       </React.StrictMode>,
       document.getElementById('root')
     );
 
 if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./components/App', renderApp)
+    module.hot.accept('./App', renderApp)
 }
 
 renderApp()
